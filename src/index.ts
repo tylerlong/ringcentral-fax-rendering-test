8053c1f58584347dd51d2d243b5d49f4ac82d02c
Tyler Long <tyler4long@gmail.com>
2021-09-09 10:38:01 -0700

    Send fax

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import RingCentral from '@rc-ex/core';
+import {Attachment} from '@rc-ex/core/lib/definitions';
+import path from 'path';
+import fs from 'fs';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -12,7 +15,25 @@ const rc = new RingCentral({
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD!,
   });
-  const extInfo = await rc.restapi().account().extension().get();
-  console.log(extInfo);
+
+  const attachment = new Attachment();
+  attachment.filename = 'index.html';
+  attachment.content = fs.createReadStream(path.join(__dirname, 'index.html'));
+  attachment.contentType = 'text/html';
+  const faxResponse = await rc
+    .restapi()
+    .account()
+    .extension()
+    .fax()
+    .post({
+      to: [
+        {
+          phoneNumber: process.env.RINGCENTRAL_RECEIVER,
+        },
+      ],
+      attachments: [attachment],
+    });
+  console.log(faxResponse);
+
   await rc.revoke();
 })();
